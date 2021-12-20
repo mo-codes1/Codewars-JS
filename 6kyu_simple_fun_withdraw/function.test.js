ee3ec169ee9c709a7e5c0a28a93405cf75f63338
Mohammed Salah <mohammed_salah@icloud.com>
2021-12-20 13:42:27 +0000

    added README

--- 6kyu_simple_fun_withdraw/function.test.js
+++ 6kyu_simple_fun_withdraw/function.test.js
@@ -4,3 +4,21 @@ const withDraw = require('./function');
 test("expects a withdrawal"  , () => {
   expect(withDraw(40)).toEqual([0,0,2]);
 });
+
+test("expects a withdrawal"  , () => {
+  expect(withDraw(250)).toEqual([2,1,0]);
+});
+
+test("expects a withdrawal"  , () => {
+  expect(withDraw(260)).toEqual([2,0,3]);
+});
+
+test("expects a withdrawal"  , () => {
+  expect(withDraw(230)).toEqual([1,1,4]);
+});
+
+test("expects a withdrawal"  , () => {
+  expect(withDraw(60)).toEqual([0,0,3]);
+});
+
+
